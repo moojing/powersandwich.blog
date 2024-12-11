f4553a715772670538571452b7eb9bffa88764a7
moojing <starts789@gmail.com>
2024-12-11 00:07:48 +0000

    feat: add contact to about me page.

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,11 @@ export const ME_AVATAR = "/avatar.jpeg";
 export const SITE_DESCRIPTION =
   "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
 
+export const CONTACTS = {
+  linkedIn: "https://www.linkedin.com/in/mu-jing-tsai/",
+  github: "https://github.com/moojing",
+  email: "mailto:moojitsai@gmail.com",
+};
 export const MENUS = [
   {
     title: "About Me",
@@ -25,11 +30,11 @@ export const MENUS = [
   },
 ];
 
-export const FOOTER_CONTENT = "A bite a day keeps bugs away.";
+export const SLOGAN = "A bite a day keeps bugs away.";
 export const ME_DESCRIPTION =
   `I am a passionate front-end developer with 4 years of experience in JavaScript and the author of a book on the
       subject. I enjoy exploring new advancements in software engineering. After transitioning from backend to frontend
-      development, I have reignited my passion for new technologies. Currently, as a front-end programmer with a deep
+      development, I have reignited my passion for new technologies. <br/> <br/> Currently, as a front-end programmer with a deep
       interest in blockchain, I am committed to contributing to this rapidly evolving field. I’ve successfully
       collaborated with developers in a Growth Squad to build experimental and innovative projects.`;
 export const GOOGLE_GTAG = "G-XXXXXXXXXX";
