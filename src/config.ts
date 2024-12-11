4879c10d209039fb8aecc5144308406d76cf0765
moojing <starts789@gmail.com>
2024-12-11 17:23:23 +0000

    feat: add projects section

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-import { Icon } from "astro-icon/components";
+import JsSandwich from './assets/projects/javascript-concept-sandwich-cover.png'
+import RaycastExt from './assets/projects/raycast-plugin.png'
+
 export const SITE_FAVICON = "/favicon.png";
 export const SITE_LOGO = "/logo.png";
 export const SITE_TITLE = "POWERSANDWICH";
@@ -37,4 +39,57 @@ export const ME_DESCRIPTION =
       development, I have reignited my passion for new technologies. <br/> <br/> Currently, as a front-end programmer with a deep
       interest in blockchain, I am committed to contributing to this rapidly evolving field. I’ve successfully
       collaborated with developers in a Growth Squad to build experimental and innovative projects.`;
+
+
+export const WORK_EXPERIENCE = [
+  {
+    company: "protto Co. Ltd.",
+    position: "Sr. Frontend Engineer",
+    startDate: "2022",
+    endDate: "2024",
+  },
+
+  {
+    company: "AppWorks Blockchain Program",
+    position: "Trainee",
+    startDate: "2023",
+    endDate: "2023",
+  },
+
+  {
+    company: "Snapask Taiwan",
+    position: "Frontend Engineer",
+    startDate: "2020",
+    endDate: "2022",
+  },
+
+  {
+    company: "Chaintech Technology",
+    position: "Frontend Engineer",
+    startDate: "2018",
+    endDate: "2020",
+  },
+];
+
+
+export const PROJECTS = [
+  {
+    title: "Publication",
+    description: "This is the first JavaScript technical book I wrote. I participated in a writing competition. In this competition, I decided to try expressing the JavaScript knowledge I had acquired. In the end, I won an award in the competition.",
+    image: JsSandwich,
+    link: "https://www.tenlong.com.tw/products/9789864347575",
+    details: [
+      "Explaining how JavaScript works through illustrations.",
+      "Making it easier for people who want to learn JavaScript.",
+      "Won an award in the competition.",
+    ],
+  },
+  {
+    title: "Raycast Extension - Coinmarketcap Price Crawler",
+    description: "A crawler to get the price of cryptocurrencies.",
+    image: RaycastExt,
+    link: "https://www.raycast.com/SonicSpark/coinmarketcap-crypto-price-crawler",
+    details:[]
+  },
+];
 export const GOOGLE_GTAG = "G-XXXXXXXXXX";
