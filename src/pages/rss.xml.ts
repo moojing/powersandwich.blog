5aad60c60ff16d1376816421b92a91518225cffe
moojing <starts789@gmail.com>
2025-01-24 12:02:00 +0000

    fix: add GET back

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,16 +3,16 @@ import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../config";
 import { CollectionType } from "../types";
 
-// export async function GET(context: any) {
-//   const posts = await getCollection(CollectionType.Blog);
-//   return rss({
-//     title: SITE_TITLE,
-//     description: SITE_DESCRIPTION,
-//     site: context.site,
-//     items: posts.map((post) => ({
-//       ...post.data,
-//       pubDate: new Date(post.data.date),
-//       link: `/${post.slug}/`,
-//     })),
-//   });
-// }
+export async function GET(context: any) {
+  // const posts = await getCollection(CollectionType.Blog);
+  // return rss({
+  //   title: SITE_TITLE,
+  //   description: SITE_DESCRIPTION,
+  //   site: context.site,
+  //   items: posts.map((post) => ({
+  //     ...post.data,
+  //     pubDate: new Date(post.data.date),
+  //     link: `/${post.slug}/`,
+  //   })),
+  // });
+}
