2c6ff109c158d4a55bccc3cc70148b6e894ab446
moojing <starts789@gmail.com>
2025-01-24 12:07:56 +0000

    chore: prevent it generate rss file.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,15 +4,15 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../config";
 import { CollectionType } from "../types";
 
 export async function GET(context: any) {
-  const posts = await getCollection(CollectionType.Blog);
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      pubDate: new Date(post.data.date),
-      link: `/${post.slug}/`,
-    })),
-  });
+  // const posts = await getCollection(CollectionType.Blog);
+  // return rss({
+  //   title: SITE_TITLE,
+  //   description: SITE_DESCRIPTION,
+  //   site: context.site,
+  //   items: posts.map((post) => ({
+  //     ...post.data,
+  //     pubDate: new Date(post.data.date),
+  //     link: `/${post.slug}/`,
+  //   })),
+  // });
 }
