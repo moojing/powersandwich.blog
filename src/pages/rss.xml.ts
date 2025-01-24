1521dfd89681a747ed5c8258c7c10edf790bb669
moojing <starts789@gmail.com>
2025-01-24 12:12:15 +0000

    fix: add rss settings back.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,20 +4,15 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../config";
 import { CollectionType } from "../types";
 
 export async function GET(context: any) {
-  // const posts = await getCollection(CollectionType.Blog);
-  // return rss({
-  //   title: SITE_TITLE,
-  //   description: SITE_DESCRIPTION,
-  //   site: context.site,
-  //   items: posts.map((post) => ({
-  //     ...post.data,
-  //     pubDate: new Date(post.data.date),
-  //     link: `/${post.slug}/`,
-  //   })),
-  // });
-  return new Response("Hello World", {
-    headers: {
-      "Content-Type": "text/plain",
-    },
+  const posts = await getCollection(CollectionType.Blog);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      pubDate: new Date(post.data.date),
+      link: `/${post.slug}/`,
+    })),
   });
 }
